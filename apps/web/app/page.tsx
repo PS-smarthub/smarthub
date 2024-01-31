72b8168b741eb206b2a5fa83209bb785cd8257be
Lopes Diego (CaP/ETS) <lod7ca@bosch.com>
2024-01-31 16:53:15 -0300

    fix docker file

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,10 @@
 import { Button } from "@smarthub/ui";
 import {Header} from '@smarthub/ui'
 
-
 export default function Page(): JSX.Element {
   return (
     <main className="bg-black min-h-screen">
-      <Button>
-        Click me!
-      </Button>
+      <Button />
       <div>
         <h1 className="text-white">Teste</h1>
       </div>
--- packages/ui/src/base/index.tsx
+++ packages/ui/src/base/index.tsx
@@ -1,2 +1,2 @@
 export * from './Header'
-export * from './button'
+export * from './Button'
