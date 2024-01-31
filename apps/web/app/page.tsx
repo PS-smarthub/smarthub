4dfb0cd2a08c77b3e59cc5444b934670ef9d866a
Lopes Diego (CaP/ETS) <lod7ca@bosch.com>
2024-01-31 14:02:16 -0300

    fix: tailwind config and repo config

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,17 @@
-import { Button } from "@repo/ui/button";
+import { Button } from "@smarthub/ui";
+import {Header} from '@smarthub/ui'
 
 
 export default function Page(): JSX.Element {
   return (
     <main className="bg-black min-h-screen">
-      <Button appName="web">
+      <Button>
         Click me!
       </Button>
+      <div>
+        <h1 className="text-white">Teste</h1>
+      </div>
+      <Header />
     </main>
   );
 }
--- packages/configs/tailwind/tailwind.config.js
+++ packages/configs/tailwind/tailwind.config.js
@@ -1,8 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
       "../../packages/ui/**/*.{js,ts,jsx,tsx}",
       "./**/*.{js,ts,jsx,tsx}",
-      "./app/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
       extend: {},
