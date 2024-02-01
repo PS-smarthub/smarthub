dc37bfebf061f16c1739f37435f943be5cc976ee
AngeloCarnevale <angelo.carnevale@hotmail.com>
2024-02-01 11:25:55 -0300

    change pnpm to npm

---
+++ apps/cold-start/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page(): JSX.Element {
+  return (
+    <div>
+      <h1 className="text-red-400">Cold-Start</h1>
+    </div>
+  );
+}
---
+++ apps/smarthub/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page(): JSX.Element {
+  return (
+    <div>
+      <h1 className="text-red-400">Smarthub</h1>
+    </div>
+  );
+}
