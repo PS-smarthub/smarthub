ec3c38af290ab7673c9497e20fdf4154baaeee74
AngeloCarnevale <angelo.carnevale@hotmail.com>
2024-02-23 15:32:40 -0300

    fix: tsconfig import

---
+++ apps/smarthub/components/footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <hr />
+      <div className="text-center">Copyright 2024 © Smarthub</div>
+    </footer>
+  );
+}
