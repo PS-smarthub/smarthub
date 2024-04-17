f303027b806584465cd77002e2d43c9b5ed03b0a
Carnevale Angelo (CaP/ETS2) <cnr6ca@bosch.com>
2024-04-17 16:40:14 -0300

    feat: oficina order form

--- apps/sot/src/app/(dashboard)/os/page.tsx
+++ apps/sot/src/app/(dashboard)/os/page.tsx
@@ -2,13 +2,13 @@
 
 import { BackButton } from "@smarthub/ui";
 import { useState } from "react";
-import SelectForm from "./_components/select-form";
+import OficinaForm from "./_components/oficina-form";
 
 export default function OSPage() {
   const [orderType, setOrderType] = useState("");
 
   return (
-    <div>
+    <>
       <BackButton
         page_name={
           orderType == "LEI"
@@ -19,18 +19,27 @@ export default function OSPage() {
         }
       />
       <div className="w-full flex">
-        <div className="w-[70%]">{orderType}</div>
-        <div className=" w-[30%]">
-          <div className="flex flex-col gap-1">
-            <label className="text-[#43464A] font-semibold">
-              Tipo de ordem
-            </label>
-            <SelectForm
-              options={[{ title: "Ordem da Oficina", value: "oficina" }]}
-            />
-          </div>
+        <div className="flex-1 pl-14 py-1 pt-6">
+          {orderType == "oficina" && <OficinaForm />}
+        </div>
+        <div className="fixed right-1">
+          {orderType == "" && (
+            <div className="flex flex-col gap-1">
+              <label className="text-[#43464A] font-semibold">
+                Tipo de ordem
+              </label>
+              <select
+                onChange={(e) => setOrderType(e.target.value)}
+                className="border rounded text-[#757575] p-2 w-[60%]"
+              >
+                <option selected>Selecione...</option>
+                <option value={"oficina"}>Ordem da Oficina</option>
+                <option value={"LEI"}>Ordem do LEI</option>
+              </select>
+            </div>
+          )}
         </div>
       </div>
-    </div>
+    </>
   );
 }
