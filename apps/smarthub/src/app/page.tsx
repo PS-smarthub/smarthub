0a219466b5484f336b00cdd80c4eecc02627917b
AngeloCarnevale <angelo.carnevale@hotmail.com>
2024-06-10 16:47:41 -0300

    feat: logo image

--- apps/smarthub/src/app/page.tsx
+++ apps/smarthub/src/app/page.tsx
@@ -1,7 +1,6 @@
 import CarouselApps from "@/components/carousel-apps";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-import Image from "next/image";
 
 export default function Home() {
   return (
